44f089417c222cb06a032a04c642008cbcac57ba
Trung Dang <60612625+dmtrung14@users.noreply.github.com>
2023-08-25 15:00:11 +0700

    rm all

--- 3Suasasm.py
+++
@@ -1,1 +0,0 @@
-class Solution:     def twoSum(self, nums: List[int], target: int) -> List[int]:         index = {}                  for i in range(len(nums)):             if target-nums[i] in index:                 return [index[target-nums[i]],i]             index[nums[i]] = i         return []
---
+++ README.md
@@ -0,0 +1,1 @@
+LeetXiv Archive Default Repository
