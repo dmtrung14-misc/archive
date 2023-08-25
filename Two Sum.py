a4e5471c878485ad63ae374263a363b5398db9ca
Trung Dang <60612625+dmtrung14@users.noreply.github.com>
2023-08-25 11:54:42 +0700

    committing files

---
+++ Two Sum.py
@@ -0,0 +1,1 @@
+class Solution:     def twoSum(self, nums: List[int], target: int) -> List[int]:         index = {}                  for i in range(len(nums)):             if target-nums[i] in index:                 return [index[target-nums[i]],i]             index[nums[i]] = i         return []
