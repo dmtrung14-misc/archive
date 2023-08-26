57c910e5fe57cf1742a1333056bd708fb9cd9cd8
Trung Dang <60612625+dmtrung14@users.noreply.github.com>
2023-08-26 18:48:58 +0700

    rebase the repository

--- New Day New Challenge.py
+++
@@ -1,1 +0,0 @@
-# lmao so hard
--- New File.py
+++
@@ -1,8 +0,0 @@
-#Interesting Codes goes here!
-try:
-            repo.create_file(path=filename, message=message, content=content, branch="main")
-            embed = discord.Embed(title=self.title, description =f"File {self.problem}.{self.language} created successfully", timestamp = datetime.now(), color = discord.Colour.blue())
-            embed.set_author(name = interaction.user, icon_url = interaction.user.avatar)
-            await interaction.response.defer()
-            await asyncio.sleep(10)
-            await interaction.followup.send(embed = embed, ephemeral=True)
